ee3ed4f3959d1073de50f51b03ac4590a1e7c3d4
nicolaparutto <nicolaparutto2003@gmail.com>
2024-12-17 17:11:21 +0100

    feat: completed last part of exercise exercise completed

--- server.js
+++ server.js
@@ -13,11 +13,17 @@ app.get('/', (req, res) => {
 //Definizione della rotta bacheca:
 app.get('/bacheca', (req,res) => {
    res.json({
-      count: posts.length,
+      elements: posts.length,
       posts: posts
    });
 });
 
+//Asset statici:
+app.use(express.static('public'));
+
+app.listen(port, () => {
+   console.log(`Sono in ascolto sulla porta ${port}`)
+})
 
 
 //Array della risposta:
@@ -25,27 +31,28 @@ const posts = [
    {
       titolo: "Torta Paesana",
       contenuto: "Torta",
-      immagine: "http://localhost:3000/",
+      immagine: "http://localhost:3000/images/torta_paesana.jpeg",
       tags: ["torta", "dolci", "cioccolato"]
    },{
       titolo: "Pasta Barbabietola",
       contenuto: "Pasta",
-      immagine: "http://localhost:3000/",
+      immagine: "http://localhost:3000/images/pasta_barbabietola.jpeg",
       tags: ["pasta", "primo", "pranzo"]
    },{
       titolo: "Pane Fritto",
       contenuto: "Pane",
-      immagine: "http://localhost:3000/",
+      immagine: "http://localhost:3000/images/pane_fritto_dolce.jpeg",
       tags: ["pane", "dolce", "fritto"]
    },{
       titolo: "Cracker Barbabietola",
       contenuto: "Cracker",
-      immagine: "http://localhost:3000/",
+      immagine: "http://localhost:3000/images/cracker_barbabietola.jpeg",
       tags: ["cracker", "snack", "salato"]
    },{
       titolo: "Ciambellone",
       contenuto: "Ciambella",
-      immagine: "http://localhost:3000/",
+      immagine: "http://localhost:3000/iamges/ciambellone.jpeg",
       tags: ["torta", "dolci", "zucchero"]
    },
 ];
+
