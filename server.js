ec7d168c61e7995ae688c538f9419eb093c13500
nicolaparutto <nicolaparutto2003@gmail.com>
2024-12-17 16:30:24 +0100

    feat: add objects array, completed second part of exercise

--- server.js
+++ server.js
@@ -7,5 +7,33 @@ const port = 3000;
 
 //Definizione della prima rotta:
 app.get('/', (req, res) => {
+   const posts = [
+      {
+         titolo: "Torta Paesana",
+         contenuto: "Torta",
+         immagine: "",
+         tags: ["torta", "dolci", "cioccolato"]
+      },{
+         titolo: "Pasta Barbabietola",
+         contenuto: "Pasta",
+         immagine: "",
+         tags: ["pasta", "primo", "pranzo"]
+      },{
+         titolo: "Pane Fritto",
+         contenuto: "Pane",
+         immagine: "",
+         tags: ["pane", "dolce", "fritto"]
+      },{
+         titolo: "Cracker Barbabietola",
+         contenuto: "Cracker",
+         immagine: "",
+         tags: ["cracker", "snack", "salato"]
+      },{
+         titolo: "Ciambellone",
+         contenuto: "Ciambella",
+         immagine: "",
+         tags: ["torta", "dolci", "zucchero"]
+      },
+   ]
    res.send('Server del mio blog')
 });
