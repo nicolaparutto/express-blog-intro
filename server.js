857f8ab1767873165ee7ded6af098297bd96518d
nicolaparutto <nicolaparutto2003@gmail.com>
2024-12-17 16:20:10 +0100

    feat: completed first part of exercise

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+//Importiamo express:
 const express = require('express');
+//Inizializziamo express invocandolo come una funzione:
 const app = express();
 const port = 3000;
+
+
+//Definizione della prima rotta:
+app.get('/', (req, res) => {
+   res.send('Server del mio blog')
+});
